ce93682ab6baed5213c2a5e92ba0903fb4ec23e2
hp <lars75@inbox.ru>
2023-04-08 01:44:37 +0300

    check

---
+++ src/loop/Task169.java
@@ -0,0 +1,28 @@
+package loop;
+
+import java.util.Scanner;
+
+public class Task169 {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int columnCount = in.nextInt();
+        String star = "*";
+        String space = " ";
+
+        for(int i = 0; i < columnCount; ++i) {
+            for(int j = 0; j < columnCount; ++j) {
+                boolean stars = j >= i && j <= columnCount - 1 - i || j <= i && j >= columnCount - 1 - i;
+                boolean spaces = i > j;
+                if (stars) {
+                    System.out.print(star);
+                } else if (spaces) {
+                    System.out.print(space);
+                }
+            }
+
+            System.out.println();
+        }
+
+    }
+}
