762257754230e2fd87afd12dd14d9d29e5d6fe9f
hp <lars75@inbox.ru>
2023-04-18 18:12:24 +0300

    check

---
+++ src/concat/ConcatExample.java
@@ -0,0 +1,12 @@
+package concat;
+
+public class ConcatExample {
+    public ConcatExample() {
+    }
+
+    public static void main(String[] args) {
+        String str = "abc".concat("def").concat("ghi").concat("jkl");
+        System.out.println(str);
+    }
+}
+
