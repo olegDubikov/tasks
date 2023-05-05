d9301e58180640d3876a5aedae1bf02ad25bbc02
hp <lars75@inbox.ru>
2023-05-05 22:24:46 +0300

    check

---
+++ src/loop/Task34.java
@@ -0,0 +1,29 @@
+package loop;
+
+import java.util.StringJoiner;
+
+public class Task34 {
+    public static void loop(int num) {
+        StringJoiner joiner = new StringJoiner(" ");
+        int count = 5;
+
+        while(count != 0) {
+            ++num;
+            if (num % 2 == 1) {
+                joiner.add(String.valueOf(num));
+                --count;
+            }
+        }
+
+        System.out.println(joiner);
+    }
+}
+
+/*
+Вывести нечетные числа
+Ниже представлен метод loop(int num), который принимает целое положительное число.
+
+Задание: Написать код, который выводит в консоль через пробел 5 последующих входящему числу нечетных чисел.
+Например, для числа 6 вывод будет:
+7 9 11 13 15
+ */
