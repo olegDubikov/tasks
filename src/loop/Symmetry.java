237177a32ad2277d1a812aad014223f8d2ba4941
hp <lars75@inbox.ru>
2023-05-20 20:42:48 +0300

    Symmetry

---
+++ src/loop/Symmetry.java
@@ -0,0 +1,25 @@
+package loop;
+
+public class Symmetry {
+    public static boolean check(int i) {
+        boolean ex = false;
+        String str = String.valueOf(i);
+        for (var x = 0; x < str.length() / 2; x++) {
+            String sStart = String.valueOf(str.charAt(x));
+            for (var y = str.length() - 1; y > str.length() / 2; y--) {
+                String sEnd = String.valueOf(str.charAt(y));
+                if (!sStart.equals(sEnd)) {
+                    ex = false;
+                    break;
+                } else {
+                    ex = true;
+                }
+            }
+        }
+        return ex;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(check(123456321));
+    }
+}
