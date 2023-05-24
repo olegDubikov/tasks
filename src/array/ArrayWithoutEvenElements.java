0269c4e86245552a11d642020341569cc29cdc9a
hp <lars75@inbox.ru>
2023-05-24 19:43:02 +0300

    ArrayWithoutEvenElements

---
+++ src/array/ArrayWithoutEvenElements.java
@@ -0,0 +1,20 @@
+package array;
+
+import java.util.Arrays;
+
+public class ArrayWithoutEvenElements {
+    public static int[] changeData(int[] data) {
+        int i = 0;
+        for (var d : data) {
+            if (d % 2 != 0) {
+                data[i++] = d;
+            }
+        }
+        return Arrays.copyOf(data, i);
+    }
+
+    public static void main(String[] args) {
+        int[] data = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        System.out.println(Arrays.toString(changeData(data)));
+    }
+}
