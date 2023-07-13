2d9a2dd4f5a6007c301f4f8acf0b0f24e090b5d8
hp <lars75@inbox.ru>
2023-07-13 14:34:46 +0300

    check

---
+++ src/arrays/CheckSquareArray.java
@@ -0,0 +1,20 @@
+package arrays;
+
+public class CheckSquareArray {
+    public static boolean checkArray(int[][] array) {
+        boolean rsl = true;
+        int standard = array.length;
+        for (int[] ints : array) {
+            if (ints.length != standard) {
+                rsl = false;
+                break;
+            }
+        }
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        int[][] in = new int[][]{{4, 5, 5, 3, 6}, {2, 3, 4, 7, 0}, {1, 2, 9, 7, 8}};
+        System.out.println(checkArray(in));
+    }
+}
