8b640e73b6cdf05a3a328c65692f3463f10458c4
олегДубиков <lars75@inbox.ru>
2023-12-24 12:37:31 +0300

    check

---
+++ src/loop/Task154.java
@@ -0,0 +1,24 @@
+package loop;
+
+import java.util.StringJoiner;
+
+public class Task154 {
+    public static void loop() {
+        StringJoiner joiner = new StringJoiner(" ");
+        for (var i = 10; i <= 20; i++){
+            for (var j = 1; j < i; j++) {
+                if(i % j == 0) {
+                    joiner.add(String.valueOf(j));
+                }
+            }
+            System.out.printf("Число: %s, делители: %s%n", i, joiner);
+            joiner = new StringJoiner(" ");
+        }
+    }
+}
+
+/*
+Вывести делители каждого числа от 10 до 20
+Задание: написать код, который выводит в консоль построчно через пробел
+все делители чисел от 10 до 20 (оба включая) в виде "Число: х, делители: у1 у2 ... уN".
+ */
